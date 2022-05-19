ddb0d37e026e464c77040fd7ef2f5838177901b2
AlexBasa <alexsimple@gmail.com>
2022-05-19 21:05:00 +0300

    More changes

--- src/aulas/Exercicios.groovy
+++ src/aulas/Exercicios.groovy
@@ -1,11 +1,13 @@
 package aulas
 
 import classes.Cliente
-import org.junit.jupiter.api.Test
+import classes.Produto
+// Criando um Alias para o Test como T
+import org.junit.jupiter.api.Test as T
 
 class Exercicios {
 
-    @Test
+    @T
     void exercicio1ponto1() {
         def nome = "Alexandre"
         def number = 18
@@ -16,7 +18,7 @@ class Exercicios {
         println number + 18
     }
 
-    @Test
+    @T
     void exercicio2() {
         Cliente cliente = new Cliente()
         cliente.setNome "Alexandre"
@@ -27,15 +29,26 @@ class Exercicios {
 
         println cliente.getNome() + cliente.getData()
     }
-    //Nao tem construtor na classe cliente
+    //Veja que na Classe Cliente não tem construtor na classe cliente
     //Aqui podemos construir mapeando o objeto
-    @Test
+
+    @T
     void exercicio3() {
         Cliente c = new Cliente(nome: "Alexandre", data: new Date())
         println c.getNome() + " " + c.getData()
         Cliente d = new Cliente(nome: "Marcelo")
         Cliente f = new Cliente(data: new Date())
 
+        d["nome"] = "Fernando"
+        println d.getNome()
+    }
+    @T
+    void exercicio4() {
+        Produto p = new Produto("Vinho", 9.00)
+        //Linha de baixo estou acessando o getNome mas pelo direct field
+        println p.nome
+        p.valor = 8.00
+        println p.nome + " " + p.valor
 
     }
 }
---
+++ src/classes/Produto.groovy
@@ -0,0 +1,12 @@
+package classes
+
+class Produto {
+
+    String nome
+    double valor
+
+    Produto(String nome, double valor){
+        this.nome = nome
+        this.valor = valor
+    }
+}
